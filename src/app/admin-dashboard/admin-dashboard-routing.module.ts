0ddc55735d9b1531cf836b581c4f70a58c976553
unknown <balavisakh@gmail.com>
2020-10-01 18:57:26 +0530

    admin-dashboad with side menu and resource allocation page is created

--- src/app/admin-dashboard/admin-dashboard-routing.module.ts
+++ src/app/admin-dashboard/admin-dashboard-routing.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminDashboardComponent } from './admin-dashboard.component';
+import { ResourceAllocationComponent } from './resource-allocation/resource-allocation.component';
 
-const routes: Routes = [{ path: '', component: AdminDashboardComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminDashboardComponent,
+    children: [
+      { path: 'resource-allocation', component: ResourceAllocationComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminDashboardRoutingModule { }
+export class AdminDashboardRoutingModule {}
--- src/app/admin-dashboard/admin-dashboard.module.ts
+++ src/app/admin-dashboard/admin-dashboard.module.ts
@@ -4,12 +4,49 @@ import { CommonModule } from '@angular/common';
 import { AdminDashboardRoutingModule } from './admin-dashboard-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard.component';
 import {DashboardModule} from '../dashboard/dashboard.module';
+import { AdminSideNavComponent } from './admin-side-nav/side-nav.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { ResourceAllocationComponent } from './resource-allocation/resource-allocation.component';
+import { MatCardModule } from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+
+
+
+
 @NgModule({
-  declarations: [AdminDashboardComponent],
+  declarations: [AdminDashboardComponent, AdminSideNavComponent, ResourceAllocationComponent],
   imports: [
     CommonModule,
     AdminDashboardRoutingModule,
-    DashboardModule
+    DashboardModule,
+    MatToolbarModule,
+    MatMenuModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatDatepickerModule
   ]
 })
 export class AdminDashboardModule { }
